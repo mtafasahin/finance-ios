12d439e25e78c41120ebe8e858dca0b5ede90fe0
mtafasahin <mtafasahin@gmail.com>
2026-02-04 20:28:39 +0300

    Initial Commit

---
+++ FinanceTracker26/FinanceTracker26App.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTracker26App.swift
+//  FinanceTracker26
+//
+//  Created by Mustafa Sahin on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct FinanceTracker26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
